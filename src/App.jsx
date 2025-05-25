4435086f058ccfdaf029ccb739ce85c5aeb2070d
moja2890 <foziahussein30@gmail.com>
2025-05-25 06:41:14 -0600

    Amazon Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./Router";
+
+
+function App() {
+  return (
+    <>
+      <AppRouter />
+      {/* <Landing /> */}
+    </>
+  );
+}
+
+export default App;
