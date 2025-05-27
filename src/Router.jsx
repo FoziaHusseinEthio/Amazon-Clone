36da2e6393e19d8a2f72eb57a618c2f954b09829
Your NameFoziaHusseinEthio <you@example.comfoziahussein30@gmail.com>
2025-05-27 01:53:55 -0600

    new

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,7 +4,7 @@ import Payment from "./pages/Payment/Payment";
 import Orders from "./pages/Orders/Orders";
 import Cart from "./pages/Cart/Cart";
 import Landing from "./pages/Landing/Landing";
-import Signup from "./pages/Auth/Signup";
+import Auth from "./pages/Auth/Auth";
 import Results from "./pages/Results/Results";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
 
@@ -12,7 +12,7 @@ const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/auth" element={<Signup />} />
+      <Route path="/auth" element={<Auth />} />
       <Route path="/payments" element={<Payment />} />
       <Route path="/orders" element={<Orders />} />
       <Route path="/category/:categoryName" element={<Results />} />
---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from'./Signup.module.css'
+
+
+function Auth() {
+  return (
+    
+     <section>Sign up</section> 
+    
+    
+  )
+}
+
+export default Auth
